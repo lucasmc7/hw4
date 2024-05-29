e09f0b040575ca66f5c5d48b9961b35c95782594
Lucas Cabrera <lucas.cabrera@kellogg.northwestern.edu>
2024-05-29 23:32:56 +0000

    pre-final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :current_user
 
+
+
   def current_user
-    puts "------------------ code before every request ------------------"
+    @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 end
